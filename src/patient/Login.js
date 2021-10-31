b8c18e8b746927be89b6081bd93979af940f5bda
Sylvester Elorm Coch <sylvester.e.coch.22@dartmouth.edu>
2021-10-31 19:52:29 -0400

    included signup page

--- src/patient/Login.js
+++ src/patient/Login.js
@@ -1,12 +1,13 @@
 import React from 'react';
+import { useHistory, Link as routerLink } from 'react-router-dom';
 import {
   Grid, Paper, TextField, Button, Typography, Link,
 } from '@mui/material';
-import { Link as routerLink } from 'react-router-dom';
 
 const Login = () => {
+  const history = useHistory();
   const paperStyle = {
-    padding: 20, height: '70vh', width: 330, margin: '5% auto',
+    padding: 20, height: 'fit-content', width: 330, margin: '5% auto',
   };
   const btnstyle = {
     margin: '8px 0',
@@ -15,16 +16,16 @@ const Login = () => {
   return (
     <Grid>
       <Paper elavation={10} style={paperStyle}>
-        <h2>Login</h2>
-        <TextField margin="normal" label="Username" placeholder="Enter username" fullWidth required />
-        <TextField margin="normal" label="Password" placeholder="Enter password" type="password" fullWidth required />
+        <h1 style={{ textAlign: 'left' }}>Login</h1>
+        <TextField size="small" margin="normal" label="Username" placeholder="Enter username" fullWidth required />
+        <TextField size="small" margin="normal" label="Password" placeholder="Enter password" type="password" fullWidth required />
         <Typography align="left">
           <Link variant="subtitle2" href="#">
             Forgot password ?
           </Link>
         </Typography>
-        <Button type="submit" color="primary" variant="contained" style={btnstyle} fullWidth>Sign in</Button>
-        <Typography variant="subtitle2" align="left"> New to Med 3.0? Create a new account <Link component={routerLink} to="/metamask"> here</Link>
+        <Button type="submit" color="primary" variant="contained" style={btnstyle} fullWidth onClick={() => history.push('/metamask')}>Sign in</Button>
+        <Typography style={{ marginTop: '16px' }} variant="subtitle2" align="left"> New to Med 3.0? Create a new account <Link component={routerLink} to="/signup"> here</Link>
         </Typography>
       </Paper>
     </Grid>
